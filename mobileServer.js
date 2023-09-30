7b872e17acfb8b3cf5e74b4f6f8933559ae990c1
sujeet_1miles <sujeet74560@gmail.com>
2023-09-30 14:43:55 +0530

    mobiles

--- mobileServer.js
+++ mobileServer.js
@@ -23,8 +23,73 @@ let {client} = require("./mobileDB.js");
 client.connect();
 
 app.get("/mobiles",function(req,res){
-    let query = `SELECT * FROM mobiles`;
-    client.query(query,function(err,results){
+    let brand = req.query.brand;
+    let ram = req.query.ram;
+    let rom = req.query.rom;
+    let os = req.query.os;
+    let options ="";
+    let optionArr = [] ;
+    let i=1;
+    if(brand){
+        let brandArr = brand.split(",");
+        options=`WHERE `;
+        let or="";
+        for(i;i<=brandArr.length;i++){
+            options+=`${or}brand=$${i}`;
+            or=" OR ";
+        }
+        optionArr=brandArr;
+    }
+    if(ram){
+        let ramArr = ram.split(",");
+        let query="";
+        let or="";
+        let num=i;
+        for(i;i<ramArr.length+num;i++){
+            query+=`${or}ram=$${i}`;
+            or=" OR ";
+        }
+        if(options){
+            options+=` AND (${query})`;
+        }else{
+        options=`WHERE ${query}`;
+        }
+        optionArr.push(...ramArr);
+    }
+    if(rom){
+        let romArr = rom.split(",");
+        let query="";
+        let or="";
+        let num=i;
+        for(i;i<romArr.length+num;i++){
+            query+=`${or}rom=$${i}`;
+            or=" OR ";
+        }
+        if(options){
+            options+=` AND (${query})`;
+        }else{
+        options=`WHERE ${query}`;
+        }
+        optionArr.push(...romArr);
+    }
+    if(os){
+        let osArr = os.split(",");
+        let query="";
+        let or="";
+        let num=i;
+        for(i;i<osArr.length+num;i++){
+            query+=`${or}os=$${i}`;
+            or=" OR ";
+        }
+        if(options){
+            options+=` AND (${query})`;
+        }else{
+        options=`WHERE ${query}`;
+        }
+        optionArr.push(...osArr);
+    }
+    let query = `SELECT * FROM mobiles ${options}`;
+    client.query(query,optionArr,function(err,results){
         if(err){
             console.log(err);
             res.status(404).send("Error in fetching data");
